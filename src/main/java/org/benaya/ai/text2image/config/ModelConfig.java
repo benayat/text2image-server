b0fddc7cafcb278070ec3fc63087a6f886a5d6a3
Benaya Trabelsi <benaya7@gmail.com>
2024-01-18 20:21:50 +0200

    .

--- src/main/java/org/benaya/ai/text2image/config/ModelConfig.java
+++ src/main/java/org/benaya/ai/text2image/config/ModelConfig.java
@@ -1,6 +1,7 @@
 package org.benaya.ai.text2image.config;
 
 import org.benaya.ai.text2image.sd4j.SD4J;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,15 +10,18 @@ import java.util.Optional;
 
 @Configuration
 public class ModelConfig {
-    private static final String modelPathParamName = "--model-path";
-    private static final String modelPath = "C:\\big-files\\test-sbai\\stable-diffusion-v1-5";
-    private static final String executionProviderParamName = "--execution-provider";
-    private static final String executionProvider = "CPU";
-    private static final String[] args = List.of(modelPathParamName, modelPath, executionProviderParamName, executionProvider).toArray(new String[0]);
+    @Value("${text2image.model-path.param-name}")
+    private String modelPathParamName;
+    @Value("${text2image.model-path.value}")
+    private String modelPath;
+    @Value("${text2image.execution-provider.param-name}")
+    private String executionProviderParamName;
+    @Value("${text2image.execution-provider.value}")
+    private String executionProvider;
 
-//      --execution-provider CPU"
     @Bean
     public SD4J.SD4JConfig sd4jConfig() {
+        String[] args = List.of(modelPathParamName, modelPath, executionProviderParamName, executionProvider).toArray(new String[0]);
         Optional<SD4J.SD4JConfig> config =  SD4J.SD4JConfig.parseArgs(args);
         if (config.isEmpty()) {
             System.out.println(SD4J.SD4JConfig.help());
